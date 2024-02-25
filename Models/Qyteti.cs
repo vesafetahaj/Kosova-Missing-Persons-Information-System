925e8521f5a60d310d5a89aba267a6af4a2f7119
Vesa <vf58160@ubt-uni.net>
2024-02-25 13:28:20 +0100

    Scaffolded identity and dbcontext

---
+++ Models/Qyteti.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KosovaMap2.Models
+{
+    public partial class Qyteti
+    {
+        public Qyteti()
+        {
+            TeDhenaVendiZhdukjesNavigations = new HashSet<TeDhena>();
+            TeDhenaVendlindjaNavigations = new HashSet<TeDhena>();
+        }
+
+        public int QytetiId { get; set; }
+        public string? QytetiEmri { get; set; }
+
+        public virtual ICollection<TeDhena> TeDhenaVendiZhdukjesNavigations { get; set; }
+        public virtual ICollection<TeDhena> TeDhenaVendlindjaNavigations { get; set; }
+    }
+}
---
+++ Models/TeDhena.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KosovaMap2.Models
+{
+    public partial class TeDhena
+    {
+        public int Id { get; set; }
+        public string? EmriMbiemri { get; set; }
+        public string? DataLindjes { get; set; }
+        public int? Vendlindja { get; set; }
+        public int? VendiZhdukjes { get; set; }
+        public string? DataZhdukjes { get; set; }
+        public string? Komente { get; set; }
+
+        public virtual Qyteti? VendiZhdukjesNavigation { get; set; }
+        public virtual Qyteti? VendlindjaNavigation { get; set; }
+    }
+}
