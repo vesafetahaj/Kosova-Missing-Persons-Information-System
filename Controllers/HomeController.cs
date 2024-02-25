fc76d2898e3f06053f617f6144758e3e750d5a57
Vesa <vf58160@ubt-uni.net>
2024-02-25 19:31:52 +0100

    CRUD of Registered Data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -59,9 +59,9 @@ namespace JobApplicationSystem.Controllers
             if (ModelState.IsValid)
             {
                 // Ensure DataZhdukjes is not later than DataLindjes
-                if (teDhena.DataZhdukjes != null && teDhena.DataLindjes != null && DateTime.Parse(teDhena.DataZhdukjes) > DateTime.Parse(teDhena.DataLindjes))
+                if (teDhena.DataZhdukjes != null && teDhena.DataLindjes != null && DateTime.Parse(teDhena.DataZhdukjes) < DateTime.Parse(teDhena.DataLindjes))
                 {
-                    ModelState.AddModelError("DataZhdukjes", "Data e Zhdukjes nuk mund të jetë më vonë se Data e Lindjes.");
+                    ModelState.AddModelError("DataZhdukjes", "Data e Zhdukjes nuk mund të jetë më heret se Data e Lindjes.");
                     ViewData["Cities"] = _context.Qytetis.ToList();
                     return View(teDhena);
                 }
@@ -73,7 +73,97 @@ namespace JobApplicationSystem.Controllers
             ViewData["Cities"] = _context.Qytetis.ToList();
             return View(teDhena);
         }
+        private bool TeDhenaExists(int id)
+        {
+            return _context.TeDhenas.Any(e => e.Id == id);
+        }
 
+        public IActionResult Update(int id)
+        {
+            var teDhena = _context.TeDhenas.Find(id);
+            if (teDhena == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Cities"] = _context.Qytetis.ToList();
+            return View(teDhena);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, [Bind("Id, EmriMbiemri, DataLindjes, Vendlindja, VendiZhdukjes, DataZhdukjes, Komente")] TeDhena teDhena)
+        {
+            if (id != teDhena.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (teDhena.DataZhdukjes != null && teDhena.DataLindjes != null && DateTime.Parse(teDhena.DataZhdukjes) < DateTime.Parse(teDhena.DataLindjes))
+                {
+                    ModelState.AddModelError("DataZhdukjes", "Data e Zhdukjes nuk mund të jetë më heret se Data e Lindjes.");
+                    ViewData["Cities"] = _context.Qytetis.ToList();
+                    return View(teDhena);
+                }
+
+                try
+                {
+                    _context.Update(teDhena);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeDhenaExists(teDhena.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Data));
+            }
+            ViewData["Cities"] = _context.Qytetis.ToList();
+            return View(teDhena);
+        }
+        public IActionResult Delete(int id)
+        {
+            var teDhena = _context.TeDhenas
+                                  .Include(t => t.VendlindjaNavigation)
+                                  .Include(t => t.VendiZhdukjesNavigation)
+                                  .FirstOrDefault(t => t.Id == id);
+            if (teDhena == null)
+            {
+                return NotFound();
+            }
+
+            return View(teDhena);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var teDhena = _context.TeDhenas
+                                   .Include(t => t.VendlindjaNavigation)
+                                   .Include(t => t.VendiZhdukjesNavigation)
+                                   .FirstOrDefault(t => t.Id == id);
+            if (teDhena == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(teDhena.VendlindjaNavigation); 
+            _context.Remove(teDhena.VendiZhdukjesNavigation); 
+
+            _context.TeDhenas.Remove(teDhena);
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Data));
+        }
 
     }
 }
